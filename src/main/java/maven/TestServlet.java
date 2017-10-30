a65286a83aed3e4187630fde3074c78796ec5634
DELL <DELL@DESKTOP-TR4OCM5>
2017-10-30 13:03:06 +0800

    20171030

--- src/main/java/maven/TestServlet.java
+++ src/main/java/maven/TestServlet.java
@@ -12,7 +12,7 @@ public class TestServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//super.doGet(req, resp);
+		super.doGet(req, resp);
 		req.getRequestDispatcher("index.jsp").forward(req, resp);
 	}
 }
